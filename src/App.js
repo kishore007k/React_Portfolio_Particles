20c859e54cc2c9d2ea5791e7ec4df6cc59bb62ea
kishore007k <kishorekumarstar07@gmail.com>
2020-08-21 17:13:43 +0530

    Resume Page 🎫 added ✔ and some Files Removed ❌

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components/Home';
+import Resume from './components/Resume'
 
 function App() {
 	return (
 		<div className="App">
       <CssBaseline />
-			<Home />
+			<Route exact path='/' component={Home} />
+			<Route exact path='/resume' component={Resume} />
 		</div>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import MobileRightMenuSlider from '@material-ui/core/Drawer';
 import {
@@ -38,19 +39,23 @@ const useStyles = makeStyles(theme => ({
 const menuItems = [
   {
     listIcon: <Home/>,
-    listText: 'Home'
+    listText: 'Home',
+    listPath: '/'
   },
   {
     listIcon: <AssignmentInd/>,
-    listText: 'Resume'
+    listText: 'Resume',
+    listPath: '/resume'
   },
   {
     listIcon: <Apps/>,
-    listText: 'Portfolio'
+    listText: 'Portfolio',
+    listPath: '/portfolio'
   },
   {
     listIcon: <ContactMail/>,
-    listText: 'Contacts'
+    listText: 'Contacts',
+    listPath: '/contacts'
   },
 ]
 
@@ -72,7 +77,7 @@ const Navbar = () => {
         <Divider />
         <List>
         {menuItems.map((listItem, key) =>(
-          <ListItem button key={key}>
+          <ListItem button key={key} component={Link} to={listItem.listPath}>
             <ListItemIcon className={classes.listItem}>
               {listItem.listIcon}
             </ListItemIcon>
